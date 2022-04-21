3ee6a6dbede52cd27238ddf71304a769d97bee11
Tyler <tiller1010@gmail.com>
2022-04-21 10:16:33 -0400

    Beautified php files

--- src/Helpers/TNTSearchHelper.php
+++ src/Helpers/TNTSearchHelper.php
@@ -4,53 +4,57 @@ namespace Werkbot\Search;
 
 use TeamTNT\TNTSearch\TNTSearch;
 use SilverStripe\Core\Environment;
+
 /**/
-class TNTSearchHelper{
+class TNTSearchHelper
+{
   /*
     Call this method to return a singleton
   */
-  public static function Instance(){
-    static $inst = null;
-    if ($inst === null) {
-      $inst = new TNTSearchHelper();
-    }
-    return $inst;
+  public static function Instance()
+  {
+      static $inst = null;
+      if ($inst === null) {
+          $inst = new TNTSearchHelper();
+      }
+      return $inst;
   }
+
   /**
    * getTNT
    *
    * @return TNTSearch
-   */
-  public function getTNTSearch(){
-    $tnt = new TNTSearch;
-    $tnt->loadConfig([
-      'driver'    => 'mysql',
-      'host'      => Environment::getEnv('SS_DATABASE_SERVER'),
-      'database'  => Environment::getEnv('SS_DATABASE_NAME'),
-      'username'  => Environment::getEnv('SS_DATABASE_USERNAME'),
-      'password'  => Environment::getEnv('SS_DATABASE_PASSWORD'),
-      'storage'   => dirname(__DIR__, 5).'/search',
-      'stemmer'   => \TeamTNT\TNTSearch\Stemmer\PorterStemmer::class
-    ]);
-    return $tnt;
+  **/
+  public function getTNTSearch()
+  {
+      $tnt = new TNTSearch;
+      $tnt->loadConfig([
+        'driver'    => 'mysql',
+        'host'      => Environment::getEnv('SS_DATABASE_SERVER'),
+        'database'  => Environment::getEnv('SS_DATABASE_NAME'),
+        'username'  => Environment::getEnv('SS_DATABASE_USERNAME'),
+        'password'  => Environment::getEnv('SS_DATABASE_PASSWORD'),
+        'storage'   => dirname(__DIR__, 5).'/search',
+        'stemmer'   => \TeamTNT\TNTSearch\Stemmer\PorterStemmer::class
+      ]);
+      return $tnt;
   }
 
   /**
    * getTNTSearch_SelectIndex
    *
    * @return TNTIndexer
-   */
-  public function getTNTSearchIndex($create=false){
-    $tnt = $this->getTNTSearch();
-    if($create){
-      $indexer = $tnt->createIndex('site.index');
-    }else{
-      $tnt->selectIndex('site.index');
-      $indexer = $tnt->getIndex();
-    }
-    $indexer->setPrimaryKey('ID');
-    return $indexer;
+  **/
+  public function getTNTSearchIndex($create = false)
+  {
+      $tnt = $this->getTNTSearch();
+      if ($create) {
+          $indexer = $tnt->createIndex('site.index');
+      } else {
+          $tnt->selectIndex('site.index');
+          $indexer = $tnt->getIndex();
+      }
+      $indexer->setPrimaryKey('ID');
+      return $indexer;
   }
-
-
 }
--- src/Tasks/SearchIndex.php
+++ src/Tasks/SearchIndex.php
@@ -6,27 +6,29 @@ use SilverStripe\Dev\BuildTask;
 use SilverStripe\Core\ClassInfo;
 use Werkbot\Search\TNTSearchHelper;
 /**/
-class SearchIndex extends BuildTask{
+class SearchIndex extends BuildTask
+{
   /**/
   protected $title = "Search Index";
   protected $description = "";
   protected $enabled = true;
   /**/
-  public function run($request){
-    if (!file_exists(dirname(__DIR__, 5).'/search')) {
-      mkdir(dirname(__DIR__, 5).'/search');
-      echo "Created search folder<br /><br />";
-    }
-    $indexer = TNTSearchHelper::Instance()->getTNTSearchIndex(true);
-    $classes = ClassInfo::classesWithExtension("Werkbot\Search\SearchableExtension");
-    foreach ($classes as $Title => $ClassName) {
-      $searchableClass = singleton($ClassName);
-      if($query = $searchableClass->getIndexQuery()){
-        echo "Indexing...$ClassName<br />";
-        $indexer->query($query);
-        $indexer->run();
-        echo "<br /><br />";
+  public function run($request)
+  {
+      if (!file_exists(dirname(__DIR__, 5).'/search')) {
+          mkdir(dirname(__DIR__, 5).'/search');
+          echo "Created search folder<br /><br />";
       }
-    }
+      $indexer = TNTSearchHelper::Instance()->getTNTSearchIndex(true);
+      $classes = ClassInfo::classesWithExtension("Werkbot\Search\SearchableExtension");
+      foreach ($classes as $Title => $ClassName) {
+          $searchableClass = singleton($ClassName);
+          if ($query = $searchableClass->getIndexQuery()) {
+              echo "Indexing...$ClassName<br />";
+              $indexer->query($query);
+              $indexer->run();
+              echo "<br /><br />";
+          }
+      }
   }
 }
