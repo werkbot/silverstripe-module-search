ec5ab88efba2200a8b7cca93a2d8422c2eecc60b
Jay Richardson <jay@werkbot.com>
2025-04-02 10:27:43 -0400

    Creates search directory if it doesn't exist
    Ensures the search directory exists before attempting to write search index files.
    
    Prevents errors on initial setup or environments where the directory is missing.
    
    Creates search index directory automatically
    
    Ensures the search index directory exists automatically upon installation.
    
    Registers the search directory and index file within composer to ensure
    they are handled correctly during project installation and updates.
    This avoids manual directory creation and ensures the index file is
    included in deployments.
    
    Exposes search index
    
    Allows public access to the search index, enabling external services or applications to directly interact with and utilize search functionalities.
    
    Creates search folder during composer install
    
    Ensures the existence of the 'search' directory by creating it during composer installation.
    
    This eliminates the need for manual folder creation and streamlines the setup process.
    Removes the extra section as it is no longer required.
    
    Adds folder creation status messages
    
    Adds messages to indicate whether a folder was created or already exists.
    
    This provides feedback to the user running the script.
    
    Updates composer scripts to use vendor path
    
    Updates the composer scripts to correctly point to the
    create-folder.php script within the vendor directory.
    This ensures the script is executed from the correct location
    after installation or updates.
    
    Creates site index file in search folder
    
    Ensures that a `site.index` file exists within the newly created
    `search` folder. This file can be used for indexing or other site-related
    purposes. It avoids creating the file if it already exists.
    
    Creates index file outside the conditional block.
    
    Ensures that the site.index file is always created in the search directory, regardless of whether the directory already exists.
    Previously, the site.index file was only created when the search directory was initially created. This change ensures that the file is created or touched every time the script runs.
    
    Creates search folder dynamically
    
    Creates the search folder during module installation
    to ensure it exists, and removes the post install script
    from composer.json since this is now handled in code.

--- _config.php
+++ _config.php
@@ -5,3 +5,11 @@ namespace Werkbot\Search;
 use SilverStripe\SiteConfig\SiteConfig;
 
 SiteConfig::add_extension(SearchSiteConfigExtension::class);
+
+// Get the root directory dynamically
+$folderPath = dirname(__DIR__, 3) . '/search';
+
+// Create the folder if it doesn't exist
+if (!is_dir($folderPath)) {
+  mkdir($folderPath, 0755, true);
+}
